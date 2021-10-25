b0d01210025d10cbbe71d0aa8906516c2dae3dfc
Gloria Da Silva <gloriadasilva@PPM-SPO-6326.local>
2021-10-25 16:59:26 -0300

    algumas libs e versions adicionadas

---
+++ buildSrc/src/main/java/Libs.kt
@@ -0,0 +1,34 @@
+object Libs {
+    // Kotlin
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlinVersion}"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayoutVersion}"
+
+    // Support
+    const val core = "androidx.core:core-ktx:${Versions.coreVersion}"
+    const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompatVersion}"
+    const val materialDesign = "com.google.android.material:material:${Versions.materialDesignVersion}"
+    const val recyclerView = "androidx.recyclerview:recyclerview:${Versions.recyclerViewVersion}"
+
+    // Retrofit
+    const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofitVersion}"
+    const val retrofitRxJava2Adapter = "com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofitRxJava2AdapterVersion}"
+    const val retrofitSerializationConverter = "com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:"
+
+    // OkHttp
+    const val okhttp = "com.squareup.okhttp3:okhttp:${Versions.okhttpVersion}"
+    const val okhttpLoggingInterceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.okhttpLoggingInterceptorVersion}"
+
+    const val kotlinSerialization = "com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:${Versions.kotlinSerializationVersion}"
+
+
+    // RxJava
+    const val rxJava = "io.reactivex.rxjava2:rxjava:${Versions.rxJavaVersion}"
+    const val rxAndroid = "io.reactivex.rxjava2:rxandroid:${Versions.rxAndroidVersion}"
+
+    // Tests
+    const val jUnit = "junit:junit:${Versions.jUnitVersion}"
+    const val jUnitAndroid = "androidx.test.ext:junit:${Versions.jUnitAndroidVersion}"
+    const val espresso = "androidx.test.espresso:espresso-core:${Versions.espressoVersion}"
+
+
+}
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,3 +1,36 @@
 object Versions {
 
+    // Kotlin
+    const val kotlinVersion = "1.5.21"
+    const val constraintLayoutVersion = ""
+
+    // Support
+    const val coreVersion = "1.6.0"
+    const val appCompatVersion = "1.3.1"
+    const val materialDesignVersion = "1.4.0"
+    const val recyclerViewVersion = "1.2.1"
+
+    // Retrofit
+    const val retrofitVersion = "2.9.0"
+    const val retrofitRxJava2AdapterVersion = "2.9.0"
+    const val retrofitGsonConverterVersion = "2.9.0"
+
+    // OkHttp
+    const val okhttpVersion = ""
+    const val okhttpLoggingInterceptorVersion = "3.12.1"
+
+    const val kotlinSerializationConverterVersion = "0.8.0"
+    const val kotlinSerializationVersion = "1.3.0"
+
+    // Gson
+    const val gsonVersion = "2.8.8"
+
+    // RxJava
+    const val rxJavaVersion = "2.2.19"
+    const val rxAndroidVersion = "2.1.1"
+
+    // Tests
+    const val jUnitVersion = "4.13.2"
+    const val jUnitAndroidVersion = "1.1.3"
+
 }
