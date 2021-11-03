319fab29a5d480e268200a07fa48340b46797001
mnmari <mariana.oliveira@picpay.com>
2021-11-03 11:47:07 -0300

    atualização das dependências do gradle e exclusão de comentários na activity do módulo app

--- app/src/main/java/com/example/storeapp/MainActivity.kt
+++ app/src/main/java/com/example/storeapp/MainActivity.kt
@@ -1,24 +1,11 @@
 package com.example.storeapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-//    private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        button = findViewById(R.id.btn)
-//        button.setOnClickListener {
-//            onClickBtnShow()
-//        }
     }
-
-//    private fun onClickBtnShow() {
-//        val intent = Intent(this, TesteRetrofitActivity::class.java)
-//        startActivity(intent)
-//    }
 }
