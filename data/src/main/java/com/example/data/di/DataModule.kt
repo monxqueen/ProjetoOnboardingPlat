0ea55996509bd202cfdefead3667b7ca6b62bfc4
mnmari <mariana.oliveira@picpay.com>
2021-12-10 12:18:05 -0300

    inclusão de di no módulo data e alterações de di no módulo favorite

---
+++ data/src/main/java/com/example/data/di/DataModule.kt
@@ -0,0 +1,25 @@
+package com.example.data.di
+
+import com.example.data.data.remote.mapper.StoresMapper
+import com.example.data.data.remote.repository.RepositoryImpl
+import com.example.data.domain.GetStoreListUseCaseImpl
+import org.koin.core.context.loadKoinModules
+import org.koin.dsl.module
+
+class DataModule {
+
+    private val modules = module {
+
+        // DATA
+        factory { StoresMapper() }
+        factory { RepositoryImpl(
+            storesMapper = get()
+        ) }
+
+        // DOMAIN
+        factory { GetStoreListUseCaseImpl(
+            repository = get()) }
+    }
+
+    fun load() = loadKoinModules(modules)
+}
