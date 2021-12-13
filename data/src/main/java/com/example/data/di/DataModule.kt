e4abd19b3852ab0fc8ac93a8415ab8f4698b188c
mnmari <mariana.oliveira@picpay.com>
2021-12-13 15:48:17 -0300

    adequação das declarações das dependências do koin com relação ao uso de factory

--- data/src/main/java/com/example/data/di/DataModule.kt
+++ data/src/main/java/com/example/data/di/DataModule.kt
@@ -4,7 +4,6 @@ import com.example.data.data.remote.mapper.StoresMapper
 import com.example.data.data.remote.repository.RepositoryImpl
 import com.example.data.domain.GetStoreListUseCase
 import com.example.data.domain.GetStoreListUseCaseImpl
-import com.example.data.domain.Repository
 import org.koin.core.context.loadKoinModules
 import org.koin.dsl.module
 
@@ -12,15 +11,11 @@ class DataModule {
 
     private val modules = module {
 
-        // DATA
-        factory { StoresMapper() }
-        factory<Repository> { RepositoryImpl(
-            storesMapper = get()
-        ) }
-
         // DOMAIN
         factory<GetStoreListUseCase> { GetStoreListUseCaseImpl(
-            repository = get()) }
+            repository = RepositoryImpl(
+                storesMapper = StoresMapper()
+            )) }
     }
 
     fun load() = loadKoinModules(modules)
--- features/favorite/src/main/java/com/example/favorite/di/FavoriteModule.kt
+++ features/favorite/src/main/java/com/example/favorite/di/FavoriteModule.kt
@@ -1,8 +1,5 @@
 package com.example.favorite.di
 
-import com.example.data.domain.GetStoreListUseCase
-import com.example.data.domain.GetStoreListUseCaseImpl
-import com.example.favorite.domain.GetFavoriteListUseCase
 import com.example.favorite.domain.GetFavoriteListUseCaseImpl
 import com.example.favorite.domain.mapper.FavoriteStoresMapper
 import com.example.favorite.presentation.FavoriteViewModel
@@ -10,20 +7,17 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.loadKoinModules
 import org.koin.dsl.module
 
-class FavoriteModule() {
+class FavoriteModule {
 
     private val modules = module {
-        // DOMAIN
-        factory { FavoriteStoresMapper() }
-        factory<GetFavoriteListUseCase> { GetFavoriteListUseCaseImpl(
-            storeListUseCase = get(),
-            favoriteStoresMapper = get())
-        }
 
         // PRESENTATION
         viewModel {
             FavoriteViewModel(
-                getFavoriteListUseCase = get())
+                getFavoriteListUseCase = GetFavoriteListUseCaseImpl(
+                    storeListUseCase = get(),
+                    favoriteStoresMapper = FavoriteStoresMapper())
+            )
         }
     }
 
