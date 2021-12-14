a50078cfb97352441a164051ceecc23af42cc2ae
gloriadasilva <gloria.silva@picpay.com>
2021-12-14 13:52:20 -0300

    retirando imports não utilizados

--- features/favorite/src/androidTest/java/com/example/favorite/FavoriteFragmentRobot.kt
+++ features/favorite/src/androidTest/java/com/example/favorite/FavoriteFragmentRobot.kt
@@ -5,10 +5,8 @@ import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.Lifecycle
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import com.example.favorite.presentation.FavoriteFragment
 
--- features/favorite/src/androidTest/java/com/example/favorite/FavoriteFragmentTest.kt
+++ features/favorite/src/androidTest/java/com/example/favorite/FavoriteFragmentTest.kt
@@ -1,21 +1,13 @@
 package com.example.favorite
 
-import androidx.lifecycle.Lifecycle
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.favorite.di.FavoriteModule
 import com.example.favorite.domain.GetFavoriteListUseCase
-import com.example.favorite.presentation.FavoriteFragment
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.core.context.loadKoinModules
-import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.recyclerview.widget.RecyclerView
-import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.matcher.ViewMatchers.*
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.dsl.module
