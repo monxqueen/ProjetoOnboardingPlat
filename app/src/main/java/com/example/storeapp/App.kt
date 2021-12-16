6f68daeb7ffc685f4f32fdfca921b8bd418492d1
Rayssa <rayssa.sousa@picpay.com>
2021-12-16 18:42:55 -0300

    exclusão de imports não utilizados

--- app/src/main/java/com/example/storeapp/App.kt
+++ app/src/main/java/com/example/storeapp/App.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import com.example.data.di.DataModule
 import com.example.favorite.di.FavoriteModule
 import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 class App : Application() {
