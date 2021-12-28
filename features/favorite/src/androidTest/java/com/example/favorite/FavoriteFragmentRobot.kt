fb81eeb599b367ed74dc2b58e86fe34722d76058
gloriadasilva <gloria.silva@picpay.com>
2021-12-28 10:59:45 -0300

    remoção do stub de erro do usecase

--- features/favorite/src/androidTest/java/com/example/favorite/FavoriteFragmentRobot.kt
+++ features/favorite/src/androidTest/java/com/example/favorite/FavoriteFragmentRobot.kt
@@ -5,15 +5,10 @@ import androidx.lifecycle.Lifecycle
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
-import com.example.favorite.remote.StubGetFavoriteListUseCaseErrorScenario
-import com.example.favorite.domain.GetFavoriteListUseCase
 import com.example.favorite.presentation.FavoriteFragment
-import org.koin.core.context.loadKoinModules
-import org.koin.dsl.module
 
 class FavoriteFragmentRobot {
     fun launchFragment() {
@@ -33,14 +28,6 @@ class FavoriteFragmentRobot {
     }
 
     private fun getView(id: Int) = onView(withId(id))
-
-    fun loadModulesOfErrorScenario() {
-        loadKoinModules(
-            module(override = true) {
-                factory<GetFavoriteListUseCase> { StubGetFavoriteListUseCaseErrorScenario }
-            }
-        )
-    }
 
     fun clickOnButton(id: Int) {
         getView(id).perform(click())
