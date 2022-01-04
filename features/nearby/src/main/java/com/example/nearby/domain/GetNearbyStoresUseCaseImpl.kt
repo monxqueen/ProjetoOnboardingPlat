8349e84494a2e2e6867cbed85567b5b56add40cf
mnmari <mariana.oliveira@picpay.com>
2022-01-04 15:03:46 -0300

    Mantendo toda a implementação do GetNearbyStoresUseCase comentada

--- features/nearby/src/main/java/com/example/nearby/domain/GetNearbyStoresUseCaseImpl.kt
+++ features/nearby/src/main/java/com/example/nearby/domain/GetNearbyStoresUseCaseImpl.kt
@@ -1,13 +1,13 @@
 package com.example.nearby.domain
 
-class GetNearbyStoresUseCaseImpl(getFavoriteListUseCase: GetFavoriteListUseCase,
-                                 getUserLocationUseCase: GetUserLocationUseCase) : GetNearbyStoresUseCase {
-
-    // Chamar os dois use cases (getFavoriteListUseCase e getUserLocationUseCase)
-    // Extrair as coordenadas do usuário a partir do getUserLocationUseCase
-    // Extrair a array de coordenadas das lojas a partir do array de lojas favoritas que o getFavoriteListUseCase retorna
-    // Para cada item do array de coordenadas de lojas favoritas, calcular a distância até o usuário (Location.distanceBetween()?) e armazenar no NearbyStores
-    // No mesmo laço do cálculo da distância, atualizar os demais campos do objeto NearbyStore (name e iconUrl)
-    // Retornar um Single desse array de objetos do tipo NearbyStores
-
-}
+//class GetNearbyStoresUseCaseImpl(getFavoriteListUseCase: GetFavoriteListUseCase,
+//                                 getUserLocationUseCase: GetUserLocationUseCase) : GetNearbyStoresUseCase {
+//
+//    // Chamar os dois use cases (getFavoriteListUseCase e getUserLocationUseCase)
+//    // Extrair as coordenadas do usuário a partir do getUserLocationUseCase
+//    // Extrair a array de coordenadas das lojas a partir do array de lojas favoritas que o getFavoriteListUseCase retorna
+//    // Para cada item do array de coordenadas de lojas favoritas, calcular a distância até o usuário (Location.distanceBetween()?) e armazenar no NearbyStores
+//    // No mesmo laço do cálculo da distância, atualizar os demais campos do objeto NearbyStore (name e iconUrl)
+//    // Retornar um Single desse array de objetos do tipo NearbyStores
+//
+//}
