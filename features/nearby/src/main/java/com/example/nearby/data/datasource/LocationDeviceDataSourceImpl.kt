7d121e5274cac37f4b3e1e41266d570778864a0e
mnmari <mariana.oliveira@picpay.com>
2022-01-17 17:54:20 -0300

    Exclusão de tratamento de erro e caracteres desnecessários

--- features/nearby/src/main/java/com/example/nearby/data/datasource/LocationDeviceDataSourceImpl.kt
+++ features/nearby/src/main/java/com/example/nearby/data/datasource/LocationDeviceDataSourceImpl.kt
@@ -9,26 +9,20 @@ import io.reactivex.Single
 internal class LocationDeviceDataSourceImpl(private val context: Context) :
     LocationDeviceDataSource {
 
-    //TODO: Levar o tratamento de permissões pra ViewModel
     @SuppressLint("MissingPermission")
     override fun getLocation(): Single<UserLocationResponse> {
 
         return Single.create { emmiter ->
             LocationServices.getFusedLocationProviderClient(context).lastLocation.addOnCompleteListener { task ->
-               try {
-                   if (task.isSuccessful) {
-                       val userLocation = UserLocationResponse(
-                           latitude = task.result.latitude,
-                           longitude = task.result.longitude
-                       // ou result ou latitude tava vindo nulo, so pra parar de quebrar
-                       )
-                       emmiter.onSuccess(userLocation)
-                   } else {
-                       emmiter.tryOnError(Exception("Não foi possível pegar a localização do dispositivo."))
-                   }
-               } catch (e: Exception){
-                   emmiter.tryOnError(Exception("Não foi possível pegar a localização do dispositivo."))
-               }
+                if (task.isSuccessful) {
+                    val userLocation = UserLocationResponse(
+                        latitude = task.result.latitude,
+                        longitude = task.result.longitude
+                    )
+                    emmiter.onSuccess(userLocation)
+                } else {
+                    emmiter.tryOnError(Exception("Não foi possível pegar a localização do dispositivo."))
+                }
             }
         }
     }
--- features/nearby/src/main/java/com/example/nearby/presentation/NearbyViewState.kt
+++ features/nearby/src/main/java/com/example/nearby/presentation/NearbyViewState.kt
@@ -5,5 +5,5 @@ import com.example.nearby.domain.entity.NearbyStores
 internal data class NearbyViewState(
     val nearbyList: List<NearbyStores>? = null,
     val isErrorVisible : Boolean = false,
-    val isLoadingVisible: Boolean = false,
+    val isLoadingVisible: Boolean = false
 )
