d356d9caccf734ba1973481c5f0e234630b26294
gloriadasilva <gloria.silva@picpay.com>
2022-01-17 17:34:29 -0300

    adicionando o factory do robot

--- features/nearby/src/androidTest/java/com/example/nearby/di/TestModule.kt
+++ features/nearby/src/androidTest/java/com/example/nearby/di/TestModule.kt
@@ -1,12 +1,17 @@
 package com.example.nearby.di
 
 import com.example.common.utils.FileReader
+import com.example.nearby.NearbyFragmentRobot
 import org.koin.core.context.loadKoinModules
 import org.koin.dsl.module
 
 internal class TestModule {
+
     private val modules = module {
+
+        factory { NearbyFragmentRobot() }
         factory { FileReader() }
+
     }
 
     fun load() = loadKoinModules(modules)
